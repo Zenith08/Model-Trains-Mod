3f8161f96b83f9ecf072732e499fd36ee6ffa450
Zenith08 <kaidenr@shaw.ca>
2019-11-14 09:12:44 -0800

    Officially updated mod to version 1.3.

--- src/main/java/me/kk47/modeltrains/Data.java
+++ src/main/java/me/kk47/modeltrains/Data.java
@@ -4,7 +4,7 @@ public class Data {
 
 	public static final String MODID = "modeltrains";
     public static final String MODNAME = "Model Trains";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.3";
     
     public static final String CLIENT_PROXY = "me.kk47.modeltrains.client.ClientProxy";
     public static final String SERVER_PROXY = "me.kk47.modeltrains.server.ServerProxy";
